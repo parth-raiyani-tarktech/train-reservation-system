8496110d4cb394e115e9a92b383ee8e5df1775f1
Yashkant103 <prasadyashkant934@gmail.com>
2024-06-02 18:41:35 +0530

    Milestone-2 Completed

--- src/edu/tutorials/trainreservation/Main.java
+++ src/edu/tutorials/trainreservation/Main.java
@@ -9,6 +9,7 @@ import edu.tutorials.trainreservation.reader.DataReader;
 import edu.tutorials.trainreservation.reader.TrainSearchRequestReader;
 import edu.tutorials.trainreservation.service.TicketBookingService;
 import edu.tutorials.trainreservation.service.TrainService;
+import edu.tutorials.trainreservation.utils.Utils;
 
 import java.util.List;
 import java.util.Scanner;
@@ -23,25 +24,35 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         while(true) {
-            System.out.println("\nEnter train search request (type 'exit' to quit)");
+            System.out.println("\nEnter train search request, PNR number or 'REPORT' (type 'exit' to quit)");
 
             String input = sc.nextLine();
             if(input.equals("exit")) {
                 break;
             }
 
-            // Search Trains
-            TrainSearchRequest trainSearchRequest = TrainSearchRequestReader.read(input);
-            System.out.println(trainSearchRequest + "\n");
+            if(input.matches("\\d+")) { // if input is a PNR number
+                long pnr = Long.parseLong(input);
+                Ticket ticket = ticketBookingService.getBookingDetails(pnr);
+                if(ticket != null) {
+                    System.out.println(ticket.getTrainNumber() + " " + ticket.getFrom() + " " + ticket.getTo() + " " + ticket.getTravelDate() + " " + ticket.getTotalFare() + " " + Utils.toCommaSeparatedSeatNo(ticket.getBookedSeats()));
+                } else {
+                    System.out.println("Invalid PNR");
+                }
+            } else if(input.equalsIgnoreCase("REPORT")) {
+                ticketBookingService.generateAndPrintReport();
+            } else { // if input is a train search request
+                TrainSearchRequest trainSearchRequest = TrainSearchRequestReader.read(input);
+                System.out.println(trainSearchRequest + "\n");
 
-            List<Train> trainsForRoute = trainService.findTrains(trainSearchRequest);
-            displayTrainNo(trainsForRoute);
+                List<Train> trainsForRoute = trainService.findTrains(trainSearchRequest);
+                displayTrainNo(trainsForRoute);
 
-            if(!trainsForRoute.isEmpty()) {
-                // Select Train Number
-                String trainNo = DataReader.readTrainNoToBookTicket();
-                Ticket bookedTicket = ticketBookingService.bookTicket(trainNo, trainSearchRequest.getCoachType(), trainSearchRequest.getTravelDate(), trainSearchRequest.getPassengerCount());
-                System.out.println("Ticket booked successfully: " + bookedTicket);
+                if(!trainsForRoute.isEmpty()) {
+                    String trainNo = DataReader.readTrainNoToBookTicket();
+                    Ticket bookedTicket = ticketBookingService.bookTicket(trainNo, trainSearchRequest.getCoachType(), trainSearchRequest.getTravelDate(), trainSearchRequest.getPassengerCount(), trainSearchRequest.getSourceCity(), trainSearchRequest.getDestinationCity());
+                    System.out.println(bookedTicket.getPnr() + " " + bookedTicket.getTotalFare() + " " + Utils.toCommaSeparatedSeatNo(bookedTicket.getBookedSeats()));
+                }
             }
         }
 
--- src/edu/tutorials/trainreservation/service/TicketBookingService.java
+++ src/edu/tutorials/trainreservation/service/TicketBookingService.java
@@ -4,6 +4,7 @@ import edu.tutorials.trainreservation.domain.CoachType;
 import edu.tutorials.trainreservation.domain.Seat;
 import edu.tutorials.trainreservation.domain.Ticket;
 import edu.tutorials.trainreservation.domain.Train;
+import edu.tutorials.trainreservation.utils.Utils;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -12,27 +13,48 @@ import java.util.List;
 public class TicketBookingService {
     private final TrainService trainService;
     private final List<Ticket> bookedTickets;
+    private long pnr = 100000000;
 
     public TicketBookingService(TrainService trainService) {
         this.trainService = trainService;
         bookedTickets = new ArrayList<>();
     }
 
-    public Ticket bookTicket(String trainNumber, CoachType coachType, LocalDate travelDate, int passengerCount) {
+    public Ticket bookTicket(String trainNumber, CoachType coachType, LocalDate travelDate, int passengerCount, String sourceCity, String destinationCity) {
         Train selectedTrain = trainService.getTrainByNumber(trainNumber);
 
         List<Seat> bookedSeats = selectedTrain.reserveSeats(coachType, travelDate, passengerCount);
-        double totalFare = coachType.calculateFare(selectedTrain.getTotalDistance(), passengerCount);
+        double totalFare = coachType.calculateFare(selectedTrain.calculateDistance(sourceCity, destinationCity), passengerCount);
 
-        long pnr = bookedTickets.size() + 1;
+        long pnr = getPNR();
 
-        Ticket ticket = new Ticket(pnr, selectedTrain.getTrainNumber(), selectedTrain.getSource(), selectedTrain.getDestination(), travelDate, totalFare, bookedSeats);
+        Ticket ticket = new Ticket(pnr, selectedTrain.getTrainNumber(), selectedTrain.getCity(sourceCity), selectedTrain.getCity(destinationCity), travelDate, totalFare, bookedSeats);
         bookedTickets.add(ticket);
 
         return ticket;
     }
 
+    private long getPNR() {
+        return ++pnr;
+    }
+
     public List<Ticket> getBookedTickets() {
         return bookedTickets;
+    }
+
+    public Ticket getBookingDetails(long pnr) {
+        for(Ticket ticket : bookedTickets) {
+            if(ticket.getPnr() == pnr) {
+                return ticket;
+            }
+        }
+        return null;
+    }
+
+    public void generateAndPrintReport() {
+        System.out.println("PNR, DATE, TRAIN, FROM, TO, FARE, SEATS");
+        for (Ticket ticket : bookedTickets) {
+            System.out.println(ticket.getPnr() + ", " + ticket.getTravelDate() + ", " + ticket.getTrainNumber() + ", " + ticket.getFrom().getName() + ", " + ticket.getTo().getName() + ", " + ticket.getTotalFare() + ", " + Utils.toCommaSeparatedSeatNo(ticket.getBookedSeats()));
+        }
     }
 }
